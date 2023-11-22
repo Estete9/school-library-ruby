26a70e2c7c6806c037f108d26ea5e15400c73a08
Esteban <cepalacios@outlook.com>
2023-11-22 11:05:50 -0500

    Update person/rb so it inherits from nameable.rb and override correct_name method

--- Person.rb
+++ Person.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
-class Person
+
+
+class Person < Nameable
   attr_reader :id
+
   attr_accessor :name, :age
 
+
+
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+
     @name = name
+
     @age = age
+
     @parent_permission = parent_permission
   end
 
+
+
   private
+
+
 
   def of_age?
     return true if @age >= 18
+
+
 
     false
   end
@@ -21,6 +36,12 @@ class Person
   def can_use_services?
     return true if of_age? || @parent_permission
 
+
+
     false
+  end
+
+  def correct_name
+    @name
   end
 end
