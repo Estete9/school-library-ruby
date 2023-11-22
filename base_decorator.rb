e88c7444f030192a5796c7de19e363fb7350624d
Esteban <cepalacios@outlook.com>
2023-11-22 11:11:21 -0500

    Imlpement BaseDecorator that inherits from Nameable

---
+++ base_decorator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
