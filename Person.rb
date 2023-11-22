9b41c2d5dfdafc70b91f4650821dd9e82c119091
Esteban <cepalacios@outlook.com>
2023-11-22 11:20:09 -0500

    Fix rubycop errors

--- Person.rb
+++ Person.rb
@@ -1,42 +1,26 @@
 # frozen_string_literal: true
 
-
-
 class Person < Nameable
   attr_reader :id
-
   attr_accessor :name, :age
-
-
 
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
-
     @name = name
-
     @age = age
-
     @parent_permission = parent_permission
   end
 
-
-
   private
-
-
 
   def of_age?
     return true if @age >= 18
-
-
 
     false
   end
 
   def can_use_services?
     return true if of_age? || @parent_permission
-
-
 
     false
   end
--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-
-
 class CapitalizeDecorator < BaseDecorator
-
   def correct_name
-
     super.upcase
-
   end
-
 end
