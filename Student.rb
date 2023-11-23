ed1d7aa6f57f67c388b1a6583589455dd36f7dac
Esteban <cepalacios@outlook.com>
2023-11-23 16:05:12 -0500

    Association one-to-many classroom-students

--- Student.rb
+++ Student.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hockey
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
