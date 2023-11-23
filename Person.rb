069cf91c94c6e916e9f4bdb6335d286005635783
Esteban <cepalacios@outlook.com>
2023-11-23 16:39:13 -0500

    Add associations Book has many Rental and Rental belongs to Book

--- Person.rb
+++ Person.rb
@@ -7,7 +7,7 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
@@ -15,6 +15,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -25,6 +26,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
   end
 
   private
--- rental.rb
+++ rental.rb
@@ -1,7 +1,18 @@
 class Rental
+  attr_reader :book, :person
   attr_accessor :date
 
   def initialize(date)
     @date = date
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals << self unless book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
   end
 end
