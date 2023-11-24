2540ea5c094151be27ac6fcf620f44ce72ae38ab
Esteban <cepalacios@outlook.com>
2023-11-24 12:08:44 -0500

    Implement student_or_teacher method that returns a Student or a Teacher object

--- app.rb
+++ app.rb
@@ -22,12 +22,40 @@ class App
     Teacher.new(age, specialization, name: name)
   end
 
+  def student_or_teacher(option)
+    if (option == 1)
+      p 'Age: '
+      age = gets.chomp
+      p 'Name: '
+      name = gets.chomp
+      p 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp
+      new_student = create_student(age, name: name, parent_permission: parent_permission)
+      p 'Student created successfully'
+      return new_student
+    end
+
+    if (option == 2)
+      p 'Age: '
+      age = gets.chomp
+      p 'Name: '
+      name = gets.chomp
+      p 'Specialization: '
+      specialization = gets.chomp
+      new_teacher = create_teacher(age, specialization, name: name)
+      p 'Student created successfully'
+      return new_teacher
+    end
+  end
+
   def print_menu
     puts 'Welcome to School Library App!'
     puts
     @menu_options.each_with_index { |option, index| puts "#{index} - #{option}" }
     user_option = gets.chomp
-
+    #here we need to implement a decision maker, for now we just call the function directly
+    student_or_teacher(user_option)
+    # add_person_to_classroom()
   end
 
   def run
